2e00561d35268c26adcba8e552667b6e650eb495
Jorge <jorgefnpimenta@gmail.com>
2017-04-21 17:32:08 +0100

    finish

--- calcuadoraCompleta/calcuadoraCompleta/Controllers/HomeController.cs
+++ calcuadoraCompleta/calcuadoraCompleta/Controllers/HomeController.cs
@@ -4,27 +4,41 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace calcuadoraCompleta.Controllers
+namespace _20170317_calculadoraCompleta.Controllers
 {
     public class HomeController : Controller
     {
+
         // GET: Home
         public ActionResult Index()
         {
-            ViewBag.visor = "0";
+
+            // inicialização de variáveis
+            ViewBag.Visor = "0";
             Session["operador"] = "";
+            Session["limpaEcra"] = true;
+
             return View();
         }
+
+
+        // POST: Home
         /// <summary>
-        /// recebe numeros  
+        /// recebe os dados da View e processa-os
+        /// com vista a calcular o resultado a apresentar
+        /// ao utilizador
         /// </summary>
         /// <returns></returns>
-
         [HttpPost]
-        public ActionResult Index(string btn, string visor)
+        public ActionResult Index(string bt, string visor)
         {
-            switch (btn)
+
+            // vars. auxiliares
+
+            // qual o valor do botão que foi pressionado?
+            switch (bt)
             {
+                case "0":
                 case "1":
                 case "2":
                 case "3":
@@ -34,43 +48,120 @@ namespace calcuadoraCompleta.Controllers
                 case "7":
                 case "8":
                 case "9":
-                case "0":
-                    if (visor.Equals("0"))
-                        visor = btn;
-                    else
-                        visor += btn;
+                    if (visor.Equals("0") || (bool)Session["limpaEcra"])
+                    {
+                        visor = bt;
+                        Session["limpaEcra"] = false;
+                    }
+                    else visor += bt; //visor = visor + bt;
                     break;
-                case "C":
-                    visor = "0";
-                    break;
+                //******************************************************
                 case ",":
-                    if (!visor.Contains(",")) visor += btn;
+                    if (!visor.Contains(",")) visor += bt;
                     break;
+                //******************************************************
                 case "+/-":
                     if (!visor.StartsWith("-")) visor = "-" + visor;
                     else visor = visor.Replace("-", "");
                     break;
+                //*************************
                 case "+":
-                    if (Convert.ToString(Session["operador"]).Equals(""))
-                    {
-                        Session["operador"] = btn;
-                        Session["operando"] = visor;
-                        visor = "";
-                    }
-                    else {
-
-                    }
-                    break;
                 case "-":
-                    break;
                 case ":":
-                    break;
                 case "x":
-                    break;
-            }
+                    // já alguém carregou num operador?
+                    string auxOperador = (string)Session["operador"];
+                    // se já alguém carregou...
+                    if (!auxOperador.Equals(""))
+                    {
+                        // já foi carregado um operador antes
+                        // vamos fazer as contas...
+                        double auxOperando1 = Convert.ToDouble(Session["operando"]);
+                        double auxOPerando2 = Convert.ToDouble(visor);
+                        switch (auxOperador)
+                        {
+                            case "+":
+                                visor = auxOperando1 + auxOPerando2 + "";
+                                break;
+                            case "-":
+                                visor = auxOperando1 - auxOPerando2 + "";
+                                break;
+                            case "x":
+                                visor = auxOperando1 * auxOPerando2 + "";
+                                break;
+                            case ":":
+                                visor = auxOperando1 / auxOPerando2 + "";
+                                break;
+                        }
+                    }
+                    // independentemente de ser a primeira vez que se carrega num operador,
+                    // ou não, há que guardar estes valores para memória futura...
+                    // preservar o valor do operador
+                    Session["operador"] = bt;
+                    // preservar o valor do visor, para memória futura
+                    Session["operando"] = visor;
+                    // marcar o visor para limpeza
+                    Session["limpaEcra"] = true;
 
-            ViewBag.visor = visor;
+                    break;
+                //******************************************************
+                case "=":
+                    // já alguém carregou num operador?
+                    auxOperador = (string)Session["operador"];
+                    // se já alguém carregou antes...
+                    if (!auxOperador.Equals(""))
+                    {
+                        // já foi carregado um operador antes
+                        // vamos fazer as contas...
+                        double auxOperando1 = Convert.ToDouble(Session["operando"]);
+                        double auxOPerando2 = Convert.ToDouble(visor);
+                        switch (auxOperador)
+                        {
+                            case "+":
+                                visor = auxOperando1 + auxOPerando2 + "";
+                                break;
+                            case "-":
+                                visor = auxOperando1 - auxOPerando2 + "";
+                                break;
+                            case "x":
+                                visor = auxOperando1 * auxOPerando2 + "";
+                                break;
+                            case ":":
+                                visor = auxOperando1 / auxOPerando2 + "";
+                                break;
+                        }
+                    }
+                    // independentemente de ser a primeira vez que se carrega num operador,
+                    // ou não, há que guardar estes valores para memória futura...
+
+                    // fazer reset ao valor do operador
+                    Session["operador"] = "";
+                    // preservar o valor do visor, para memória futura
+                    Session["operando"] = visor;
+                    // marcar o visor para limpeza
+                    Session["limpaEcra"] = true;
+
+                    break;
+                //******************************************************
+                case "C":
+                    // fazer reset ao valor do operador
+                    Session["operador"] = "";
+                    // fazer reset ao valor do visor
+                    Session["operando"] = "";
+                    // marcar o visor para limpeza
+                    Session["limpaEcra"] = true;
+                    // limpar o visor
+                    visor = "0";
+                    break;
+            } // fim do switch() que faz a gestão dos botões
+
+
+            // preparar os dados para serem enviados para a View
+            ViewBag.Visor = visor;
+
             return View();
-        }
+        } // fim do método INDEX (em ambiente Http.POST)
+
+
     }
 }
